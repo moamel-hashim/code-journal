61911718a81207018d64d0eb102172063e32fe61
moamel97 <muamal1997@gmail.com>
2021-10-26 16:57:33 -0700

    completed the first feature

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
 /* global data */
 /* exported data */
+var $form = document.querySelector('form');
+var $imgSrc = document.querySelector('img');
+var $photoUrl = document.querySelector('#URL');
+$photoUrl.addEventListener('input', addURL);
+$form.addEventListener('submit', submitForm);
+
+function addURL(event) {
+  var imageSource = $photoUrl.value;
+  $imgSrc.setAttribute('src', imageSource);
+  if ($photoUrl.value === '') {
+    $imgSrc.setAttribute('src', './images/placeholder-image-square.jpg');
+  }
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  var messageData = {
+    title: $form.elements.title.value,
+    image: $form.elements.image.value,
+    notes: $form.elements.notes.value,
+    entryId: data.nextEntryId++
+  };
+  $imgSrc.setAttribute('src', './images/placeholder-image-square.jpg');
+  data.entries.unshift(messageData);
+  $form.reset();
+}
