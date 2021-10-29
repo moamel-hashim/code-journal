71f87fb68f9e82afc491840a60eeff03127fa992
moamel97 <muamal1997@gmail.com>
2021-10-28 19:33:05 -0700

    completed some tasks

--- js/main.js
+++ js/main.js
@@ -27,81 +27,76 @@ function submitForm(event) {
   $form.reset();
 }
 
-//  <div data-view="entries">
-//       <div class="row align-items justify">
-//         <div class="">
-//           <h1>entries</h1>
-//         </div>
-//         <div class="column-half padding-0 text-align">
-//           <button>NEW</button>
-//         </div>
-//       </div>
-//       <ul class="row">
-//         <li class="column-full padding-0">
-//           <div class="row">
-//             <div class="column-half">
-//               <img src="./images/placeholder-image-square.jpg" alt="placeholder-image-square">
-//             </div>
-//             <div class="column-half">
-//               <h2>Lorem, ipsum.</h2>
-//               <p>Lorem ipsum dolor sit amet
-//                 consectetur adipisicing elit. Quis, ut?</p>
-//             </div>
-//           </div>
-//         </li>
-//         <li class="column-full padding-0">
-//           <div class="row">
-//             <div class="column-half">
-//               <img src="./images/placeholder-image-square.jpg" alt="placeholder-image-square">
-//             </div>
-//             <div class="column-half">
-//               <h2>Lorem, ipsum.</h2>
-//               <p>Lorem ipsum dolor sit amet consectetur,
-//                 adipisicing elit. Blanditiis, perspiciatis!</p>
-//             </div>
-//           </div>
-//         </li>
-//       </ul>
-//     </div>
+// var $main = document.querySelector('main');
+// var $divEntries = document.querySelector('.entries');
+var $ul = document.querySelector('ul');
+// $divEntries.appendChild($ul);
 
-var $main = document.querySelector('main');
-
-function entriesNotes(event) {
-  var $ul = document.createElement('ul');
-  $ul.setAttribute('class', 'row');
-  // li
+function entriesNotes(entry) {
+  // debugger;
   var $li = document.createElement('li');
   $li.setAttribute('class', 'column-full padding-0');
   $ul.appendChild($li);
-  // div for the row
+
   var $divRow = document.createElement('div');
   $divRow.setAttribute('class', 'row');
   $li.appendChild($divRow);
-  // div for the column-half
+
   var $divColumnHalf = document.createElement('div');
   $divColumnHalf.setAttribute('class', 'column-half');
-  $li.appendChild($divColumnHalf);
-  // img
+  $divRow.appendChild($divColumnHalf);
+
   var $imgholder = document.createElement('img');
-  $imgholder.setAttribute('src', '');
+  $imgholder.setAttribute('src', entry.image);
   $divColumnHalf.appendChild($imgholder);
-  // div second column-half
+
   var $secondDivColumnHalf = document.createElement('div');
   $secondDivColumnHalf.setAttribute('class', 'column-half');
-  $li.appendChild($secondDivColumnHalf);
-  // h2
+  $divRow.appendChild($secondDivColumnHalf);
+
   var $h2 = document.createElement('h2');
-  $h2.textContent = '';
+  $h2.textContent = entry.title;
   $secondDivColumnHalf.appendChild($h2);
-  // p
+
   var $p = document.createElement('p');
-  $p.textContent = '';
+  $p.textContent = entry.notes;
   $secondDivColumnHalf.appendChild($p);
+  return $li;
 }
 
-window.addEventListener('DOMContentLoaded', entriesNotes);
+window.addEventListener('DOMContentLoaded', handleDomLoaded);
 
-for (var i = 0; i < length; i++) {
-  var displayNotes = entriesNotes([i]);
-  $main.appendChild(displayNotes);
+function handleDomLoaded(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    var displayNotes = entriesNotes(data.entries[i]);
+    $ul.appendChild(displayNotes);
+  }
+}
+
+var $newButton = document.querySelector('.second-button');
+$newButton.addEventListener('click', newButton);
+
+function newButton(event) {
+  var newEntry = event.target.getAttribute('data-view');
+  switchView(newEntry);
+}
+
+var $anchor = document.querySelector('a');
+$anchor.addEventListener('click', anchor);
+
+function anchor(event) {
+  var entryView = event.target.getAttribute('data-view');
+  switchView(entryView);
+}
+
+var $view = document.querySelectorAll('.view');
+
+function switchView(viewName) {
+  for (var i = 0; i < $view.length; i++) {
+    if ($view[i].getAttribute('data-view') === viewName) {
+      $view[i].className = 'view';
+    } else {
+      $view[i].className = 'hidden';
+    }
+  }
 }
